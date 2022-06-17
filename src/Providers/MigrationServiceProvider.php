68fa21810519a7566590bd06a3e9c47cb08d84e7
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-06-17 03:53:40 -0300

    First commit

---
+++ src/Providers/MigrationServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WellingtonCarneiroBarbosa\LaravelCustomMigrationsPaths\Providers;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class MigrationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__ . '/../Config/config.php' => config_path('custom_migrations.php'),
+        ], 'custom-migrations-paths');
+
+        $this->registerMigrations();
+    }
+
+    private function registerMigrations(): void
+    {
+        $this->getBaseMigrationsPaths()->each(function ($basePath) {
+            $this->getMigrationsPathsChild($basePath)->each(function ($path) use ($basePath) {
+                $this->loadMigrationsFrom($basePath . DIRECTORY_SEPARATOR . $path);
+            });
+        });
+    }
+
+    protected function getBaseMigrationsPaths(): Collection
+    {
+        return collect(config('custom_migrations.base_paths'))->map(function ($baseMigrationPath) {
+            return base_path($baseMigrationPath);
+        });
+    }
+
+    protected function getMigrationsPathsChild(string $basePath): Collection
+    {
+        return collect(scandir($basePath))->filter(function ($path) {
+            return !in_array($path, config('custom_migrations.ignore_paths'));
+        });
+    }
+}
